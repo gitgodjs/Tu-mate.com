ac518756896c473129d64ab539de5576fa3295ea
Juan Roman <maestroxd4@gmail.com>
2024-07-16 00:49:52 -0300

    Probando conexion con Ubuntu

--- Frontend/src/Paginas/Inicio.tsx
+++ Frontend/src/Paginas/Inicio.tsx
@@ -1,8 +1,8 @@
 export default function Inicio() {
     return (
         <>
-            <h1>Hola!</h1>
-            <h2>Mundo!</h2>
+            <h1>Hola!!</h1>
+            <h2>Mundo!!!</h2>
         </>
     );
 }
