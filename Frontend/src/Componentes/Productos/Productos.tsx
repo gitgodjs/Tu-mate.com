ec31a2d4fb435cdcf13060a78782f6bb48740972
gitgodjs <maestroxd4@gmail.com>
2024-08-21 12:31:04 -0300

    Probando nueva configuracion

--- Frontend/src/Componentes/Productos/Productos.tsx
+++ Frontend/src/Componentes/Productos/Productos.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import obtenerProductos from "./ObtenerProductos";
 import { Producto } from "../../Types/types";
 import Filtros from "./Filtros";
-import TodosProds from "./Todos";
+import { Link } from "react-router-dom";
 
 export default function Productos() {
     const [listaProductos, setListaProductos] = useState<Producto[]>([]);
@@ -34,7 +34,28 @@ export default function Productos() {
     return (
         <div>
             <Filtros filtro={setFiltro} orden={setOrden}/>
-            <TodosProds prod_filtrados={prod_filtrados} />
+            <section id="Productos sueltos" className="grid grid-cols-2 gap-2 m-1 p-1 sm:grid-cols-4">
+            {prod_filtrados.map((producto, key)=>(
+                
+                <div className='grid grid-cols-1 bg-gray-100 rounded-md p-2 producto' key={key}>
+                    <div id="img" className='flex items-center justify-center h-64'>
+                        <img src={producto.imageUrl} alt={producto.name} 
+                        className='rounded-md w-full h-full object-cover'/>
+                    </div>
+                    <div id="text" className="flex flex-col justify-left m-1">
+                        <h2 className='text-left text-lg font-bold'>{producto.name}</h2>
+                        <p>{`${producto.descripcion.split(" ").slice(0, 9).join(" ")}...`}</p>
+                        <span>Precio: {producto.precio}</span>
+                    </div>
+                    <div id="Boton" className="grid grid-cols-1 items-center justify-left m-1">
+                        <Link to={`/Productos/${producto._id}`}
+                        className="border-none text-center text-white font-medium bg-blue-600 text-xl p-2 rounded-md cursor-pointer duration-1000 hover:bg-yellow-400 ">
+                            Ver más
+                        </Link>
+                    </div>
+                </div>
+            ))}
+            </section>
         </div>
     );
 }
--- Frontend/src/Paginas/Pag-Producto.tsx
+++ Frontend/src/Paginas/Pag-Producto.tsx
@@ -25,28 +25,35 @@ export default function ProductoUnico(){
     }, [id]);
 
     return(
-        <div className="grid grid-cols-1 bg-gray-100 sm:grid-cols-2 shadow-2xl gap-2 borde m-3 p-3 w-auto h-auto rounded-md">
-            {producto.map((prod, key)=>(
-                <div key={key}>
-                    <section>
-                        <img src={prod.imageUrl} alt={prod.name} />
-                    </section>
-                    <section className="relative flex flex-col h-auto p-2">
-                    <h2 className="text-3xl font-medium">{prod.name}</h2>
-                    <p className="text-1xl mt-4 sm:text-2xl">{prod.descripcion}</p>
-                    <div className="flex gap-2">
-                        <p className="flex text-xl mt-5">Precio: ${prod.precio}</p>
-                        <input className="flex" type="number" min={1} max={9}/>
+        <div>
+            <div >
+                {producto.map((prod, key)=>(
+                    <div key={key} className="grid grid-cols-1 bg-gray-100 sm:grid-cols-2 shadow gap-2 borde m-4 p-3 w-auto h-auto rounded-md">
+                        <section>
+                            <img src={prod.imageUrl} alt={prod.name} className="object-cover h-96 w-full"/>
+                        </section>
+                        <section className="relative flex flex-col h-auto p-2">
+                            <h2 className="text-3xl font-medium">{prod.name}</h2>
+                            <p className="text-1xl mt-4 sm:text-2xl">{prod.descripcion}</p>
+                            <div className="flex gap-2">
+                                <p className="flex text-xl mt-5">Precio: ${prod.precio}</p>
+                                <input className="flex" type="number" min={1} max={9}/>
+                            </div>
+
+                            <div className="items-center mt-5  pointer text-white aling-center text-center bg-blue-600 p-2 font-medium text-xl rounded-md">
+                                <button className="m-1">Agregar al carrito</button>
+                            </div>
+                        </section>
                     </div>
-                    
-                    <div className="items-center mt-5  pointer text-white aling-center text-center bg-blue-600 p-2 font-medium text-xl rounded-md">
-                        <button className="m-1">Agregar al carrito</button>
-                    </div>
-                </section>
-                </div>
-            ))}
-            <Productos />
+                ))}
+            </div>
+            <div className='border-t border-black'></div>
+            <div className='mt-3'>
+               <Productos /> 
+            </div>
+            
         </div>
+        
         
     )
 }
